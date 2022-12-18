fe21e6011a754793da59bcdc1d8f6dcfba842fef
Rohan3476 <120353864+Rohan3476@users.noreply.github.com>
2022-12-18 10:53:34 -0800

    Add files via upload

---
+++ src/main/java/frc/subsystem/Intake.java
@@ -0,0 +1,33 @@
+package frc.subsystem;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.Solenoid;
+import static frc.utility.Pneumatics.getPneumaticsHub;
+
+public class Intake extends AbstractSubsystem{
+    private final Solenoid intakeSol;
+    private TalonFX intakeMotorFalcon;
+    private static Intake instance = new Intake();
+    public Intake() {
+        super(-1, 4);
+        intakeSol = getPneumaticsHub().makeSolenoid(3);
+    }
+
+    @Override
+    public void close() throws Exception {
+        intakeSol.close();
+        instance = new Intake();
+    }
+
+    public void toggle() {
+        intakeSol.toggle();
+    }
+
+    public void setIntakeMotorFalcon(double speed) {
+        intakeMotorFalcon.set(ControlMode.PercentOutput, speed);
+    }
+
+    public static Intake getInstance() {
+        return instance;
+    }
+}
